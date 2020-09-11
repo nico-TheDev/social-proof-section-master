dfacd19e76cc648c428123166981c3cee91494df
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-09-12 01:06:59 +0800

    initial commit 🎉

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    purge: [],
+    future: {
+        removeDeprecatedGapUtilities: true,
+        purgeLayersByDefault: true,
+    },
+    theme: {
+        extend: {
+            colors: {
+                darkMagenta: "hsl(300, 43%, 22%)",
+                softPink: "hsl(333, 80%, 67%)",
+                darkGrayMagenta: "hsl(303, 10%, 53%)",
+                lightGrayMagenta: "hsl(300, 24%, 96%)",
+            },
+            fontFamily: {
+                spartan: ["Spartan", "sans-serif"],
+            },
+            fontSize: {
+                default: "15px",
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+};
